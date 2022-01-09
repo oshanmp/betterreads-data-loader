71318aec3a6af93232548971beaf7021c4bb4ffe
oshanmp <32547741+oshanmp@users.noreply.github.com>
2022-01-09 13:12:15 +0530

    Initial commit.

---
+++ src/test/java/com/oshan/betterreads/BetterreadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oshan.betterreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
